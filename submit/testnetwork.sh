d8f039fd70228786fd9adb7d8867e0d4490b2de6
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-08-27 16:48:09 -0500

    Fix reference to python script

--- submit/testnetwork.sh
+++ submit/testnetwork.sh
@@ -3,7 +3,7 @@ if [ -e /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash ];
 then
     source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
     module load python/2.7
-    ./testnetwork.sh
+    ./testnetwork.py
 else
-    ./testnetwork.sh
+    ./testnetwork.py
 fi
